3dae402092bc4b1fa238a695acda1b8a1064a6ba
Ben Brown <ben.brown@codethink.co.uk>
2020-06-25 11:27:09 +0100

    Fix wget calls in download script
    * Remove duplicate 'wget' arg.
    * Remove erroneous '.' arg.

--- download-test-package.sh
+++ download-test-package.sh
@@ -2,5 +2,5 @@
 
 mkdir -p xtensor/osx-64
 mkdir -p xtensor/linux-64
-wget wget https://conda.anaconda.org/conda-forge/osx-64/xtensor-0.16.1-0.tar.bz2 -P xtensor/osx-64/ .
-wget wget https://conda.anaconda.org/conda-forge/linux-64/xtensor-0.16.1-0.tar.bz2 -P xtensor/linux-64/ .
+wget https://conda.anaconda.org/conda-forge/osx-64/xtensor-0.16.1-0.tar.bz2 -P xtensor/osx-64/
+wget https://conda.anaconda.org/conda-forge/linux-64/xtensor-0.16.1-0.tar.bz2 -P xtensor/linux-64/
