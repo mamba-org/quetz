a262b8ffa69ba30c395c52743cac3f69c10f91f4
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2020-07-14 12:07:37 +0200

    feat: setup.py for use in conda recipe

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import setuptools
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+version_ns = {}
+with open(os.path.join(here, 'quetz', '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+
+setuptools.setup(
+    name="quetz",
+    version=version_ns['__version__'],
+    author="The Quetz Development Team",
+    description="The Mamba package server",
+    long_description="The Mamba package server",
+    url="https://github.com/thesnakepit/quetz",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
