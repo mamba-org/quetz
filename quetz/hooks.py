ff38323c679584a074e0d9d151dd6ab90115f98b
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-10-30 17:40:48 +0100

    updating plugin docs

--- quetz/hooks.py
+++ quetz/hooks.py
@@ -1,13 +1,30 @@
+import fastapi
 import pluggy
+
+import quetz
 
 hookspec = pluggy.HookspecMarker("quetz")
 
 
 @hookspec
-def register_router():
-    """add extra endpoints to the tree"""
+def register_router() -> 'fastapi.APIRouter':
+    """add extra endpoints to the url tree.
+
+    It should return an :py:class:`fastapi.APIRouter` with new endpoints definitions.
+    By default it will be added to the root of the urlscheme"""
 
 
 @hookspec
-def post_add_package_version(version, condainfo):
-    """hook for post-processsing after adding a package version"""
+def post_add_package_version(
+    version: 'quetz.db_models.PackageVersion', condainfo: 'quetz.condainfo.CondaInfo'
+) -> None:
+    """hook for post-processsing after adding a package file.
+
+    Arguments:
+    :param quetz.db_models.PackageVersion version:
+        package version model that was added in to the database
+
+    :param quetz.condainfo.CondaInfo condainfo:
+        metadata extracted from the archive
+
+    """
