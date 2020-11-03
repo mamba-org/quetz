92f690db2eb8c740a640981e520d937c64db3587
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-11-03 19:07:39 +0100

    first implementation of repodata patching

--- plugins/quetz_repodata_patching/quetz_repodata_patching/main.py
+++ plugins/quetz_repodata_patching/quetz_repodata_patching/main.py
@@ -1,11 +1,50 @@
+import json
+import tarfile
+from contextlib import contextmanager
+
 import quetz
+from quetz.db_models import PackageVersion
+from quetz.deps import get_db
 
 
 @quetz.hookimpl
 def post_package_indexing(
     pkgstore: "quetz.pkgstores.PackageStore", channel_name, subdirs
 ):
-    for subdir in subdirs:
-        fs = pkgstore.serve_path(channel_name, f"{subdir}/repodata.json")
-        data = fs.read()
-        pkgstore.add_file(data, channel_name, f"{subdir}/repodata_from_packages.json")
+
+    get_db_manager = contextmanager(get_db)
+    with get_db_manager() as db:
+
+        query = (
+            db.query(PackageVersion)
+            .filter(
+                PackageVersion.channel_name == channel_name,
+                PackageVersion.package_name == f"{channel_name}-repodata-patches",
+            )
+            .order_by(PackageVersion.version.desc())
+        )
+        patches_pkg = query.one_or_none()
+
+    if patches_pkg:
+        fs = pkgstore.serve_path(channel_name, "noarch/" + patches_pkg.filename)
+
+        with tarfile.open(mode='r:bz2', fileobj=fs) as patch_archive:
+            for subdir in subdirs:
+                patch_instructions = json.load(
+                    patch_archive.extractfile(f"{subdir}/patch_instructions.json")
+                )
+                fs = pkgstore.serve_path(channel_name, f"{subdir}/repodata.json")
+
+                repodata_str = fs.read()
+                repodata = json.loads(repodata_str)
+
+                pkgstore.add_file(
+                    repodata_str, channel_name, f"{subdir}/repodata_from_packages.json"
+                )
+
+                for pkg, info in patch_instructions["packages"].items():
+                    repodata["packages"][pkg].update(info)
+
+                pkgstore.add_file(
+                    json.dumps(repodata), channel_name, f"{subdir}/repodata.json"
+                )
--- plugins/quetz_repodata_patching/tests/test_main.py
+++ plugins/quetz_repodata_patching/tests/test_main.py
@@ -1,18 +1,196 @@
+import json
 import os
+import uuid
+from unittest import mock
 
+import pytest
+
+import quetz
 from quetz import indexing
+from quetz.db_models import Channel, Package, Profile, User
 
 
-def test_post_package_indexing(config, dao):
+@pytest.fixture
+def user(db):
+    user = User(id=uuid.uuid4().bytes, username="bartosz")
+    profile = Profile(name="Bartosz", avatar_url="http:///avatar", user=user)
+    db.add(user)
+    db.add(profile)
+    db.commit()
+    yield user
 
+
+@pytest.fixture
+def channel_name():
+    return "my-channel"
+
+
+@pytest.fixture
+def package_name():
+    return "mytestpackage"
+
+
+@pytest.fixture
+def package_file_name(package_name):
+    return f"{package_name}-0.1-0.tar.bz"
+
+
+@pytest.fixture
+def channel(dao: "quetz.dao.Dao", channel_name, user):
+
+    channel_data = Channel(name=channel_name, private=False)
+    channel = dao.create_channel(channel_data, user.id, "owner")
+    return channel
+
+
+@pytest.fixture
+def package_version(
+    dao: "quetz.dao.Dao", user, channel, package_name, db, package_file_name
+):
+    package_data = Package(name=package_name)
+
+    dao.create_package(channel.name, package_data, user.id, "owner")
+    package_format = 'tarbz2'
+    package_info = '{"run_exports": {"weak": ["otherpackage > 0.1"]}, "size": 100}'
+    version = dao.create_version(
+        channel.name,
+        package_name,
+        package_format,
+        "noarch",
+        "0.1",
+        "0",
+        "0",
+        package_file_name,
+        package_info,
+        user.id,
+    )
+
+    yield version
+
+
+@pytest.fixture
+def repodata_name(channel):
+    package_name = f"{channel.name}-repodata-patches"
+    return package_name
+
+
+@pytest.fixture
+def repodata_file_name(repodata_name):
+    version = "0.1"
+    build_str = "0"
+    return f"{repodata_name}-{version}-{build_str}.tar.bz2"
+
+
+@pytest.fixture
+def patch_content(package_file_name):
+    return {
+        "packages": {
+            package_file_name: {"run_exports": {"weak": ["otherpackage > 0.2"]}}
+        }
+    }
+
+
+@pytest.fixture
+def repodata_archive(repodata_file_name, patch_content):
+
+    import tarfile
+    import time
+    from io import BytesIO
+
+    tar_content = BytesIO()
+    tar = tarfile.open(repodata_file_name, "w|bz2", fileobj=tar_content)
+
+    patch_instructions = BytesIO(json.dumps(patch_content).encode('ascii'))
+
+    info = tarfile.TarInfo(name='noarch')
+    info.type = tarfile.DIRTYPE
+    info.mode = 0o755
+    info.mtime = time.time()
+    tar.addfile(tarinfo=info)
+
+    info = tarfile.TarInfo(name='noarch/patch_instructions.json')
+    info.size = len(patch_instructions.getvalue())
+    info.mtime = time.time()
+    tar.addfile(tarinfo=info, fileobj=patch_instructions)
+    tar.close()
+    tar_content.seek(0)
+    yield tar_content
+
+
+@pytest.fixture
+def package_repodata_patches(
+    dao: "quetz.dao.Dao",
+    user,
+    channel,
+    db,
+    pkgstore,
+    repodata_name,
+    repodata_file_name,
+    repodata_archive,
+):
+
+    package_name = repodata_name
+    package_data = Package(name=package_name)
+
+    dao.create_package(channel.name, package_data, user.id, "owner")
+    package_format = 'tarbz2'
+    package_info = '{"size": 100}'
+    version = dao.create_version(
+        channel.name,
+        package_name,
+        package_format,
+        "noarch",
+        "0.1",
+        "0",
+        "0",
+        repodata_file_name,
+        package_info,
+        user.id,
+    )
+
+    pkgstore.add_package(repodata_archive, channel.name, f"noarch/{repodata_file_name}")
+
+    return version
+
+
+@pytest.fixture
+def pkgstore(config):
     pkgstore = config.get_package_store()
-    indexing.update_indexes(dao, pkgstore, "my_channel")
+    return pkgstore
 
-    assert os.path.isfile(
-        os.path.join(pkgstore.channels_dir, "my_channel", "noarch", "repodata.json")
+
+def test_post_package_indexing(
+    pkgstore,
+    dao,
+    package_version,
+    channel_name,
+    package_repodata_patches,
+    db,
+    package_file_name,
+):
+    def get_db():
+        yield db
+
+    with mock.patch("quetz_repodata_patching.main.get_db", get_db):
+        indexing.update_indexes(dao, pkgstore, channel_name)
+
+    repodata_path = os.path.join(
+        pkgstore.channels_dir, channel_name, "noarch", "repodata.json"
     )
-    assert os.path.isfile(
-        os.path.join(
-            pkgstore.channels_dir, "my_channel", "noarch", "repodata_from_packages.json"
-        )
+    assert os.path.isfile(repodata_path)
+    with open(repodata_path) as fid:
+        data = json.load(fid)
+    assert data['packages'][package_file_name]['run_exports'] == {
+        "weak": ["otherpackage > 0.2"]
+    }
+
+    orig_repodata_path = os.path.join(
+        pkgstore.channels_dir, channel_name, "noarch", "repodata_from_packages.json"
     )
+
+    assert os.path.isfile(orig_repodata_path)
+    with open(orig_repodata_path) as fid:
+        data = json.load(fid)
+    assert data['packages'][package_file_name]['run_exports'] == {
+        "weak": ["otherpackage > 0.1"]
+    }
