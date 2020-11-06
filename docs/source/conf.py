baa541ad789c3c27aaf1dadc21fd1380f60f4ce6
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-11-06 14:33:47 +0100

    updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,7 @@ exclude_patterns = []
 html_theme = 'sphinx_book_theme'
 html_logo = "_static/quetz_logo.png"
 html_title = "documentation"
+html_favicon = "_static/quetz_favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
