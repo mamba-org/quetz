a38a00f4b49dfd72d4fd1897c0db1fa795564c19
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-11-13 12:38:05 +0100

    add config tests

--- quetz/tests/test_config.py
+++ quetz/tests/test_config.py
@@ -1,5 +1,6 @@
 import pytest
 
+from quetz.config import Config
 from quetz.dao import Dao
 
 
@@ -20,8 +21,27 @@ def test_config_users(config):
     assert not config.users_members
 
 
-@pytest.mark.xfail
-def test_config_user_role(dao: Dao, user, config):
-    user = dao.get_user_by_username(user.username)
+@pytest.mark.parametrize(
+    "config_extra", ["[users]\nadmins=[]", "[users]\nmaintainers=[]"]
+)
+def test_config_empty_users_section(dao: Dao, user, config):
 
-    assert user.role == 'member'
+    assert config.configured_section("users")
+    assert not config.users_admins
+    assert not config.users_maintainers
+    assert not config.users_members
+    assert not config.users_default_role
+    assert not config.users_create_default_channel
+
+
+def test_config_is_singleton(config):
+
+    c = Config()
+
+    assert c is config
+
+    Config._instance = None
+
+    c_new = Config()
+
+    assert c_new is not config
