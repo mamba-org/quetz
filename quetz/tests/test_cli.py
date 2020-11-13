29732964edd3c6a3271d0663dfe76bd804a78980
Adrien DELSALLE <ad.delsalle@gmail.com>
2020-11-13 18:16:53 +0100

    add a test when no user is provided
    Signed-off-by: Adrien DELSALLE <ad.delsalle@gmail.com>

--- quetz/tests/test_cli.py
+++ quetz/tests/test_cli.py
@@ -13,9 +13,12 @@ def user_group():
 
 @pytest.fixture
 def config_extra(user_group):
-    return f"""[users]
-    {user_group} = ["bartosz"]
-    """
+    if user_group is None:
+        return ""
+    else:
+        return f"""[users]
+                {user_group} = ["bartosz"]
+                """
 
 
 @pytest.mark.parametrize(
@@ -23,6 +26,7 @@ def config_extra(user_group):
     [("admins", "owner"), ("maintainers", "maintainer"), ("members", "member")],
 )
 def test_init_db(db, config, config_dir, user_group, expected_role):
+
     def get_db(_):
         return db
 
@@ -38,7 +42,20 @@ def test_init_db(db, config, config_dir, user_group, expected_role):
     assert not user.profile
 
 
-def test_init_db_user_exists(db, config, user, config_dir):
+@pytest.mark.parametrize("user_group", [None])
+def test_init_db_no_users(db, config, config_dir, user_group):
+    def get_db(_):
+        return db
+
+    with mock.patch("quetz.cli.get_session", get_db):
+        cli.init_db(config_dir)
+
+    user = db.query(User).filter(User.username == "bartosz").one_or_none()
+
+    assert user is None
+
+
+def test_init_db_user_exists(db, config, config_dir, user):
     def get_db(_):
         return db
 
