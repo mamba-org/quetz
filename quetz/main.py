4db689d457f75ee85b26dee7a6c97f93f52b3b52
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-11-17 15:03:35 +0100

    minor fix

--- quetz/main.py
+++ quetz/main.py
@@ -388,7 +388,7 @@ def get_channel(channel: db_models.Channel = Depends(get_channel_allow_proxy)):
 
 
 def can_channel_synchronize(channel):
-    return channel.mirror_mode == "mirror"
+    return channel.mirror_channel_url and (channel.mirror_mode == "mirror")
 
 
 def can_channel_reindex(channel):
