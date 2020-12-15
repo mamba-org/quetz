20752dd573dbb7064c988fb3b183cf16965d3be0
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-12-15 18:17:55 +0100

    adding test for package versions

--- quetz/tests/test_db_models.py
+++ quetz/tests/test_db_models.py
@@ -3,8 +3,14 @@
 # Distributed under the terms of the Modified BSD License.
 
 import uuid
+from pathlib import Path
 
-from quetz.db_models import User
+import pytest
+
+from quetz.config import Config
+from quetz.dao import Dao
+from quetz.db_models import PackageVersion, User
+from quetz.rest_models import Channel, Package
 
 
 def test_user(db):
@@ -21,3 +27,66 @@ def test_user(db):
 
     db.delete(user)
     db.commit()
+
+
+@pytest.fixture
+def version_factory(
+    db,
+    user,
+    dao: Dao,
+    config: Config,
+):
+    channel_name = "test-channel"
+    package_name = "test-package"
+    dao.create_channel(
+        Channel(name=channel_name, private=False), user_id=user.id, role="owner"
+    )
+    dao.create_package(
+        channel_name, Package(name=package_name), user_id=user.id, role="owner"
+    )
+
+    def factory(version):
+        filename = Path(f"test-package-{version}-0.tar.bz2")
+        package_format = "tarbz2"
+        package_info = "{}"
+        package_version = dao.create_version(
+            channel_name,
+            package_name,
+            package_format,
+            "linux-64",
+            str(version),
+            0,
+            "",
+            str(filename),
+            package_info,
+            user.id,
+            size=11,
+        )
+        return package_version
+
+    return factory
+
+
+v = PackageVersion.version
+
+
+@pytest.mark.parametrize(
+    "versions,query,expected_versions",
+    [
+        (["0.1", "0.2"], v < "0.2", {"0.1"}),
+        (["0.11", "0.2"], v < "0.2", {"0.11"}),
+        (["0.2.0", "0.12.0", "1.0.0"], v < "0.2", {"0.12.0"}),
+        (["0.2.0", "0.12.0", "1.0.0"], v < "0.12", set()),
+    ],
+)
+def test_package_version_comparison(
+    version_factory, db, versions, query, expected_versions
+):
+    for version in versions:
+        version_factory(version)
+
+    packages = db.query(PackageVersion).filter(query).all()
+
+    obtained_versions = {p.version for p in packages}
+
+    assert expected_versions == obtained_versions
