0104ffb2612f2ea05275e784cb9be85853da581f
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-21 08:53:09 +0100

    fix transmuting to right inner file names

--- plugins/quetz_transmutation/quetz_transmutation/api.py
+++ plugins/quetz_transmutation/quetz_transmutation/api.py
@@ -1,9 +1,10 @@
 import json
+import logging
 import os
 import pickle
 import shutil
 from pathlib import Path
-from tempfile import NamedTemporaryFile
+from tempfile import TemporaryDirectory
 
 from conda_package_handling.api import _convert
 from fastapi import APIRouter, Depends
@@ -18,6 +19,7 @@ from quetz.pkgstores import PackageStore
 from .rest_models import PackageSpec
 
 router = APIRouter()
+logger = logging.getLogger("quetz-cli")
 
 
 def transmutation(package_version: dict, config, pkgstore: PackageStore, dao: Dao):
@@ -37,43 +39,44 @@ def transmutation(package_version: dict, config, pkgstore: PackageStore, dao: Da
 
     fh = pkgstore.serve_path(channel, Path(platform) / filename)
 
-    with NamedTemporaryFile("wb", delete=False, suffix=".tar.bz2") as local_file:
-        # chunk size 10MB
-        shutil.copyfileobj(fh, local_file, 10 * 1024 * 1024)
+    with TemporaryDirectory() as tmpdirname:
+        local_file_name = os.path.join(tmpdirname, filename)
+        with open(local_file_name, "wb") as local_file:
+            # chunk size 10MB
+            shutil.copyfileobj(fh, local_file, 10 * 1024 * 1024)
 
-    tempdir = os.path.dirname(local_file.name) + os.sep
-    fn, out_fn, errors = _convert(local_file.name, ".conda", tempdir, force=True)
+        fn, out_fn, errors = _convert(local_file_name, ".conda", tmpdirname, force=True)
 
-    if errors:
-        print("ERROR: transmutation errors --> ", errors)
-        return
+        if errors:
+            logger.error(f"transmutation errors --> {errors}")
+            return
 
-    filename_conda = os.path.basename(filename).replace('.tar.bz2', '.conda')
+        filename_conda = os.path.basename(filename).replace('.tar.bz2', '.conda')
 
-    print("Adding file to package store: ", Path(platform) / filename_conda)
+        logger.info(f"Adding file to package store: {Path(platform) / filename_conda}")
 
-    with open(out_fn, 'rb') as f:
-        calculate_file_hashes_and_size(info, f)
-        f.seek(0)
-        pkgstore.add_package(f, channel, Path(platform) / filename_conda)
+        with open(out_fn, 'rb') as f:
+            calculate_file_hashes_and_size(info, f)
+            f.seek(0)
+            pkgstore.add_package(f, channel, Path(platform) / filename_conda)
 
-    version = dao.create_version(
-        channel,
-        package_name,
-        "conda",
-        platform,
-        version,
-        build_number,
-        build_string,
-        filename_conda,
-        json.dumps(info),
-        uploader_id,
-        info["size"],
-        upsert=True,
-    )
+        version = dao.create_version(
+            channel,
+            package_name,
+            "conda",
+            platform,
+            version,
+            build_number,
+            build_string,
+            filename_conda,
+            json.dumps(info),
+            uploader_id,
+            info["size"],
+            upsert=True,
+        )
 
-    if os.path.exists(out_fn):
-        os.remove(out_fn)
+        if os.path.exists(out_fn):
+            os.remove(out_fn)
 
 
 transmutation_serialized = pickle.dumps(transmutation)
