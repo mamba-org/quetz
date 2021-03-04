e57d777037c52a9e974c8c3d21d9d4918df94e73
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2021-03-04 10:43:28 +0100

    logging progress using logger

--- plugins/quetz_harvester/quetz_harvester/jobs.py
+++ plugins/quetz_harvester/quetz_harvester/jobs.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from pathlib import Path
 from typing import Optional
 
@@ -7,6 +8,8 @@ from pydantic import BaseModel, Field
 
 from quetz.dao import Dao
 from quetz.pkgstores import PackageStore
+
+logger = logging.getLogger("quetz.plugins")
 
 
 class PackageSpec(BaseModel):
@@ -18,21 +21,20 @@ def harvest(package_version: dict, config, pkgstore: PackageStore, dao: Dao):
     channel: str = package_version["channel_name"]
     platform = package_version["platform"]
 
-    print(f"Harvesting: {filename}, {channel}, {platform}")
+    logger.debug(f"Harvesting: {filename}, {channel}, {platform}")
     # TODO figure out how to handle properly either .conda or .tar.bz2
     if not filename.endswith('.tar.bz2'):
         return
 
     fh = pkgstore.serve_path(channel, Path(platform) / filename)
 
-    print("Harvesting ... ")
     try:
         result = libc_harvest(fh)
     except Exception as e:
-        print(f"Exception caught in harvesting: {str(e)}")
+        logger.exception(f"Exception caught in harvesting {filename}: {str(e)}")
         return
 
-    print(f"Uploading harvest result for {channel}/{platform}/{filename}")
+    logger.debug(f"Uploading harvest result for {channel}/{platform}/{filename}")
 
     pkgstore.add_file(
         json.dumps(result, indent=4, sort_keys=True),
