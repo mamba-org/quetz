4917e30ef77173bce0c3fc689db1d8c06912ec33
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-04-12 16:27:45 +0200

    fix tests

--- plugins/quetz_mamba_solve/tests/test_mamba_solve.py
+++ plugins/quetz_mamba_solve/tests/test_mamba_solve.py
@@ -1,3 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def plugins():
+    return ["quetz-mamba_solve"]
+
+
 def test_mamba_solve_endpoint(client):
     response = client.post(
         "/api/mamba/solve",
@@ -8,13 +16,8 @@ def test_mamba_solve_endpoint(client):
         },
     )
     assert response.status_code == 200
-    assert (
-        response.content
-        == """# platform: osx-64
+    assert b"@EXPLICIT" in response.content
+    assert response.content.startswith(b"# platform: osx-64")
 
-@EXPLICIT
-
-https://conda.anaconda.org/conda-forge/osx-64/libcxx-11.1.0-habf9029_0.tar.bz2#a88609e545f948a404419c65cd96fa1a
-https://conda.anaconda.org/conda-forge/osx-64/xtensor-0.23.4-h940c156_0.tar.bz2#4e01e80d87ddf3867715989f2d8cec7c
-https://conda.anaconda.org/conda-forge/osx-64/xtl-0.7.2-h940c156_1.tar.bz2#57a0019a4f48cdd2f902cde9ca9d0fba"""  # noqa: E501
-    )
+    assert b"conda-forge/osx-64/libcxx-" in response.content
+    assert b"conda-forge/osx-64/xtensor-" in response.content
