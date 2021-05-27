550a5535ce57ed6a619f00faf7f568ad9568815d
Madhur Tandon <madhur16053@iiitd.ac.in>
2021-05-27 16:21:48 +0530

    add separate upload endpoints for LocalStore

--- plugins/quetz_conda_trust/quetz_conda_trust/api.py
+++ plugins/quetz_conda_trust/quetz_conda_trust/api.py
@@ -2,8 +2,8 @@ import os
 
 from fastapi import APIRouter, UploadFile, File, Depends
 
-from quetz import authorization, db_models
-from quetz.deps import get_rules, ChannelChecker
+from quetz import authorization
+from quetz.deps import get_rules
 from quetz.config import Config
 
 from tempfile import SpooledTemporaryFile
@@ -13,23 +13,33 @@ config = Config()
 
 pkgstore = config.get_package_store()
 
-@router.post("/api/channels/{channel_name}/conda-trust/files/root", status_code=201, tags=["files"])
+def post_file(file):
+    if type(file.file) is SpooledTemporaryFile and not hasattr(file, "seekable"):
+        file.file.seekable = file.file._file.seekable
+
+    file.file.seek(0, os.SEEK_END)
+    file.file.seek(0)
+
+    # channel_name is passed as "" (empty string)
+    # since we want to upload the file in a host-wide manner i.e. independent of individual channels
+    # this hack only works for LocalStore since Azure and S3 necessarily require the creation of
+    # `containers` and `buckets` (mapped to individual channels) before we can upload a file there.
+    pkgstore.add_file(file.file.read(), "", file.filename)
+
+
+@router.post("/api/conda-trust/upload-root", status_code=201, tags=["files"])
 def post_root_json_to_channel(
-    channel_name,
     root_json_file: UploadFile = File(...),
-    channel: db_models.Channel = Depends(
-        ChannelChecker(allow_proxy=False, allow_mirror=False)
-    ),
     auth: authorization.Rules = Depends(get_rules),
 ):
-    user_id = auth.assert_user()
-    auth.assert_upload_file(channel.name, root_json_file.filename)
+    auth.assert_server_roles(["owner", "maintainer"])
+    post_file(root_json_file)
 
-    if type(root_json_file.file) is SpooledTemporaryFile and not hasattr(root_json_file, "seekable"):
-        root_json_file.file.seekable = root_json_file.file._file.seekable
 
-    root_json_file.file.seek(0, os.SEEK_END)
-    root_json_file.file.seek(0)
-
-    pkgstore.add_file(root_json_file.file.read(), "", root_json_file.filename)
-    
+@router.post("/api/conda-trust/upload-key-mgr", status_code=201, tags=["files"])
+def post_root_json_to_channel(
+    key_mgr_file: UploadFile = File(...),
+    auth: authorization.Rules = Depends(get_rules),
+):
+    auth.assert_server_roles(["owner", "maintainer"])
+    post_file(key_mgr_file)
