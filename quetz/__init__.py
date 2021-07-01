640b37e84a6798f8177ab694fdcb5dd803d79e35
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-07-01 17:31:24 +0200

    add micromamba to path in CI

--- quetz/__init__.py
+++ quetz/__init__.py
@@ -1,7 +1,8 @@
 # Copyright 2020 QuantStack
 # Distributed under the terms of the Modified BSD License.
 try:
-	import pluggy
-	hookimpl = pluggy.HookimplMarker("quetz")
+    import pluggy
+
+    hookimpl = pluggy.HookimplMarker("quetz")
 except ImportError:
-	pass
+    pass
