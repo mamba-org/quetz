30ceda7cec93f9ae03b2684c119ae17d66eff322
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-07-01 17:11:27 +0200

    move to setup.cfg and release 0.1.0

--- quetz/__init__.py
+++ quetz/__init__.py
@@ -1,5 +1,7 @@
 # Copyright 2020 QuantStack
 # Distributed under the terms of the Modified BSD License.
-import pluggy
-
-hookimpl = pluggy.HookimplMarker("quetz")
+try:
+	import pluggy
+	hookimpl = pluggy.HookimplMarker("quetz")
+except ImportError:
+	pass
