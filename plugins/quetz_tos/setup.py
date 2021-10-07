2a2759c15d5a29b29c3e777b57198763967cf2b8
Madhur Tandon <madhur16053@iiitd.ac.in>
2021-10-07 15:21:48 +0530

    add migrations for tables

--- plugins/quetz_tos/setup.py
+++ plugins/quetz_tos/setup.py
@@ -6,6 +6,11 @@ setup(
     entry_points={
         "quetz": ["quetz-tos = quetz_tos.main"],
         "quetz.models": ["quetz-tos = quetz_tos.db_models"],
+        "quetz.migrations": ["quetz-tos = quetz_tos.migrations"],
     },
-    packages=["quetz_tos"],
+    packages=[
+        "quetz_tos",
+        "quetz_tos.migrations",
+        "quetz_tos.migrations.versions",
+    ],
 )
