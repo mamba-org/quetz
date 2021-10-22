954d9a5374261ae9afd697fb5339b71427f906e6
Madhur Tandon <madhur16053@iiitd.ac.in>
2021-10-22 16:57:54 +0530

    fail test if user not found

--- plugins/quetz_tos/quetz_tos/main.py
+++ plugins/quetz_tos/quetz_tos/main.py
@@ -17,31 +17,36 @@ def check_for_signed_tos(user_id, user_role):
     with get_db_manager() as db:
         dao = Dao(db)
         user = dao.get_user(user_id)
-        if user_role == OWNER:
-            return True
-        else:
-            selected_tos = (
-                db.query(TermsOfService)
-                .order_by(TermsOfService.time_created.desc())
-                .first()
-            )
-            if selected_tos:
-                signature = (
-                    db.query(TermsOfServiceSignatures)
-                    .filter(TermsOfServiceSignatures.user_id == user_id)
-                    .filter(TermsOfServiceSignatures.tos_id == selected_tos.id)
-                    .one_or_none()
-                )
-                if signature:
-                    return True
-                else:
-                    detail = f"terms of service is not signed for {user.username}"
-                    raise HTTPException(
-                        status_code=status.HTTP_403_FORBIDDEN, detail=detail
-                    )
-            else:
+        if user:
+            if user_role == OWNER:
                 return True
-
+            else:
+                selected_tos = (
+                    db.query(TermsOfService)
+                    .order_by(TermsOfService.time_created.desc())
+                    .first()
+                )
+                if selected_tos:
+                    signature = (
+                        db.query(TermsOfServiceSignatures)
+                        .filter(TermsOfServiceSignatures.user_id == user_id)
+                        .filter(TermsOfServiceSignatures.tos_id == selected_tos.id)
+                        .one_or_none()
+                    )
+                    if signature:
+                        return True
+                    else:
+                        detail = f"terms of service is not signed for {user.username}"
+                        raise HTTPException(
+                            status_code=status.HTTP_403_FORBIDDEN, detail=detail
+                        )
+                else:
+                    return True
+        else:
+            detail = f"user with id {user_id} not found"
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=detail
+            )
 
 @quetz.hookimpl
 def check_additional_permissions(user_id, user_role):
