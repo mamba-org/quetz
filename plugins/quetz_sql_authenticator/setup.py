d12b04ae485db84679048248c66c95853e558774
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2022-03-23 13:04:57 +0100

    lint

--- plugins/quetz_sql_authenticator/setup.py
+++ plugins/quetz_sql_authenticator/setup.py
@@ -4,9 +4,7 @@ from setuptools import setup
 
 setup(
     name="quetz-sql-authenticator",
-    install_requires=[
-        "sqlmodel"
-    ],
+    install_requires=["sqlmodel"],
     entry_points={
         "quetz.authenticator": [
             "sql-authenticator = quetz_sql_authenticator:SQLAuthenticator"
