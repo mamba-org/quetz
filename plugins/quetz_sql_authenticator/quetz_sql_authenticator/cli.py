240bdbc1cc1b7171ce50bb9f7c94865da0c3b836
Simon Bohnen <simonmbohnen@gmail.com>
2022-10-10 12:02:24 +0000

    Stop using sqlmodel

--- plugins/quetz_sql_authenticator/quetz_sql_authenticator/cli.py
+++ plugins/quetz_sql_authenticator/quetz_sql_authenticator/cli.py
@@ -2,7 +2,7 @@ import os
 
 import click
 from passlib.hash import pbkdf2_sha256
-from sqlmodel import Session, SQLModel, create_engine, select
+from quetz.database import get_db_manager
 
 from .db_models import Credentials
 
@@ -20,72 +20,64 @@ def _cli():
 @_cli.command("create")
 @click.argument("username")
 @click.argument("password")
-@click.option(
-    "--database-url",
-    help="The SQLAlchemy URL pointing to the database.",
-    default=os.environ.get("QUETZ_SQL_AUTHENTICATOR_DATABASE_URL"),
-)
-def _create(username: str, password: str, database_url: str) -> None:
+def _create(username: str, password: str) -> None:
     credentials = Credentials(username=username, password=calculate_hash(password))
-    with Session(create_engine(database_url)) as session:
-        session.add(credentials)
-        session.commit()
+    with get_db_manager() as db:
+        db.add(credentials)
+        db.commit()
     click.echo(f"INFO: User '{username}' created successfully.")
 
 
 @_cli.command("update")
 @click.argument("username")
 @click.argument("password")
-@click.option(
-    "--database-url",
-    help="The SQLAlchemy URL pointing to the database.",
-    default=os.environ.get("QUETZ_SQL_AUTHENTICATOR_DATABASE_URL"),
-)
-def _update(username: str, password: str, database_url: str) -> None:
-    statement = select(Credentials).where(Credentials.username == username)
-    with Session(create_engine(database_url)) as session:
-        credentials = session.exec(statement).one()
+def _update(username: str, password: str) -> None:
+    with get_db_manager() as db:
+        credentials = (
+            db.query(Credentials)
+            .filter(Credentials.username == username)
+            .one_or_none()
+        )
+        if credentials is None:
+            raise click.ClickException(f"ERROR: User '{username}' not found.")
         credentials.password = calculate_hash(password)
-        session.commit()
+        db.commit()
     click.echo(f"INFO: User '{username}' successfully updated.")
 
 
 @_cli.command("delete")
 @click.argument("username")
-@click.option(
-    "--database-url",
-    help="The SQLAlchemy URL pointing to the database.",
-    default=os.environ.get("QUETZ_SQL_AUTHENTICATOR_DATABASE_URL"),
-)
-def _delete(username: str, database_url: str) -> None:
-    statement = select(Credentials).where(Credentials.username == username)
-    with Session(create_engine(database_url)) as session:
-        credentials = session.exec(statement).one()
-        session.delete(credentials)
-        session.commit()
+def _delete(username: str) -> None:
+    with get_db_manager() as db:
+        credentials = (
+            db.query(Credentials)
+            .filter(Credentials.username == username)
+            .one_or_none()
+        )
+        if credentials is None:
+            raise click.ClickException(f"ERROR: User '{username}' not found.")
+        db.delete(credentials)
+        db.commit()
     click.echo(f"INFO: User '{username}' successfully deleted.")
 
 
 @_cli.command("reset")
-@click.option(
-    "--database-url",
-    help="The SQLAlchemy URL pointing to the database.",
-    default=os.environ.get("QUETZ_SQL_AUTHENTICATOR_DATABASE_URL"),
-)
 def _reset(database_url: str) -> None:
-    engine = create_engine(database_url)
-    statement = select(Credentials)
-    with Session(engine) as session:
-        credentials = list(session.exec(statement))
-    click.echo(f"WARNING: Resetting the table will delete {len(credentials)} users.")
+    with get_db_manager() as db:
+        credentials_count = (
+            db.query(Credentials)
+            .count()
+        )
+    click.echo(f"WARNING: Resetting the table will delete {credentials_count} users.")
     while (
         reset_database := input("Are you sure you want to reset the table? [Y/n]")
     ) not in ("Y", "n"):
         pass
     else:
         if reset_database == "Y":
-            SQLModel.metadata.drop_all(engine)
-            SQLModel.metadata.create_all(engine)
+            with get_db_manager() as db:
+                db.query(Credentials).delete()
+                db.commit()
             click.echo("INFO: Table reset successful.")
         else:
             click.echo("INFO: Table reset aborted.")
