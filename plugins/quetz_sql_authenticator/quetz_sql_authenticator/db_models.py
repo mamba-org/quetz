022fdbee315c4e755476e908f8f81671e35a79a2
Simon Bohnen <simonmbohnen@gmail.com>
2022-10-25 14:38:04 +0000

    Add new alembic migration

--- plugins/quetz_sql_authenticator/quetz_sql_authenticator/db_models.py
+++ plugins/quetz_sql_authenticator/quetz_sql_authenticator/db_models.py
@@ -7,4 +7,4 @@ class Credentials(Base):
     __tablename__ = "credentials"
 
     username = Column("username", String, primary_key=True)
-    password_hash = Column("password", String, nullable=False)
+    password_hash = Column("password_hash", String, nullable=False)
