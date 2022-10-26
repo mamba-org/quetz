bbbc5e7c32d1e5da6903e3d0855014c09a2aed5e
Simon Bohnen <simonmbohnen@gmail.com>
2022-10-26 15:55:38 +0000

    Fix update route

--- plugins/quetz_sql_authenticator/quetz_sql_authenticator/api.py
+++ plugins/quetz_sql_authenticator/quetz_sql_authenticator/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.session import Session
 
 from quetz import authorization
@@ -72,8 +73,16 @@ def _create(
         username=username, password_hash=_calculate_hash(password)
     )
     db.add(credentials)
-    db.commit()
-
+    try:
+        db.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"""
+            A database error occured.
+            This is most likely due to the user {username} already existing.
+            """,
+        )
     return username
 
 
@@ -95,7 +104,7 @@ def _update(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"User {username} not found",
         )
-    credentials.password = _calculate_hash(password)
+    credentials.password_hash = _calculate_hash(password)
     db.commit()
 
     return username
