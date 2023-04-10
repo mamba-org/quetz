c449d068c3b1220d216e93b7bc25bdfcd624ba95
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-04-10 12:12:25 +0200

    Add basic readiness and liveness checks.

--- quetz/main.py
+++ quetz/main.py
@@ -1839,4 +1839,14 @@ def serve_channel_index(
     return serve_path("index.html", channel, accept_encoding, session, dao)
 
 
+@app.get("/health/ready", status_code=status.HTTP_200_OK)
+def is_ready(task: Task = Depends(get_tasks_worker)):
+    return {}
+
+
+@app.get("/health/live", status_code=status.HTTP_200_OK)
+def is_live(task: Task = Depends(get_tasks_worker)):
+    return {}
+
+
 frontend.register(app)
