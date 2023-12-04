1d2bd0f8e5dcb6373bc76343d4bb3eda3975c359
Robin Holzinger <robin.holzinger@quantco.com>
2023-12-04 11:11:37 +0100

    remove commented out code

--- quetz/tests/test_config.py
+++ quetz/tests/test_config.py
@@ -3,7 +3,6 @@ import os
 import tempfile
 
 import pytest
-
 from quetz.config import Config, ConfigEntry, ConfigSection, configure_logger
 from quetz.dao import Dao
 from quetz.errors import ConfigError
